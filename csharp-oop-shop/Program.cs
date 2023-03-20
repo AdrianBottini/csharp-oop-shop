34890af6d65fc8d8a039112950992f87630a9b57
adrianboh@gmail.com <adrianboh@gmail.com>
2023-03-20 16:10:19 +0100

    esercizio finito

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -3,3 +3,10 @@
 var product1 = new Product("Computer", "gioco qualsiasi per bambino qualsiasi", 224.95);
 var product2 = new Product("Videogame", "gioco qualsiasi per bambino qualsiasi", 50.90);
 var product3 = new Product("Cuffie", "gioco qualsiasi per bambino qualsiasi", 78.75);
+var line = Environment.NewLine;
+
+Console.WriteLine($"Prodotto: {product1.ExtendedName() + line}" + $"Descrizione: {line + product1.Description + line}" + $"Prezzo senza iva: {product1.Price + line} " + $"Prezzo con iva: {product1.TotalPrice()} ");
+Console.WriteLine("");
+Console.WriteLine($"Prodotto: {product2.ExtendedName() + line}" + $"Descrizione: {line + product2.Description + line}" + $"Prezzo senza iva: {product2.Price + line} " + $"Prezzo con iva: {product2.TotalPrice()} ");
+Console.WriteLine("");
+Console.WriteLine($"Prodotto: {product3.ExtendedName() + line}" + $"Descrizione: {line + product3.Description + line}" + $"Prezzo senza iva: {product3.Price + line} " + $"Prezzo con iva: {product3.TotalPrice()} ");
