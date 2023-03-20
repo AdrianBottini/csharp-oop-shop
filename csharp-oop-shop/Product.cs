efd4065378163bb03022359c7dd56cfe17142187
adrianboh@gmail.com <adrianboh@gmail.com>
2023-03-20 15:39:13 +0100

    inizializzazioni prodotti

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace csharp_oop_shop
+{
+    internal class Product
+    {
+        private int code;
+        private string name;
+        private string description;
+        private double price;
+        private double iva;
+
+
+
+        public Product(string name, string description, double price)
+        {
+            var random = new Random();
+            this.code = random.Next(100, 999);
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.iva = price * 0.22;
+        }
+
+        public int Code { get => code; }
+        public string Name { get => name; set { name = value; } }
+        public string Description { get => description; set { description = value; } }
+        public double Price
+        {
+            get => price; set
+            {
+                price = value;
+            }
+        }
+        public double Iva { get => iva; }
+
+        public double TotalPrice()
+        {
+            return price + this.iva;
+        }
+
+        public string ExtendedName()
+        {
+            return $"{Code} - {Name}";
+        }
+    }
+}
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp_oop_shop;
+
+var product1 = new Product("Computer", "gioco qualsiasi per bambino qualsiasi", 224.95);
+var product2 = new Product("Videogame", "gioco qualsiasi per bambino qualsiasi", 50.90);
+var product3 = new Product("Cuffie", "gioco qualsiasi per bambino qualsiasi", 78.75);
